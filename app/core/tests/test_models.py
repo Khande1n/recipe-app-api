da2fa0fbe931f264dab651daa6d1373dd17961cd
khande2n <nikhil.khandelwal@talentedge.in>
2020-08-20 23:11:52 +0530

    custom user changes to admin

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -16,3 +16,25 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test new user email is normalized"""
+        email = "nikhil@NIKHIL.COM"
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test new user invalid email raises value error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_new_superuser_create(self):
+        """Test new superuser is created"""
+        user = get_user_model().objects.create_superuser(
+            'nikhil@nikhil.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
